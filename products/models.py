2680859aa449b7b8a46acf9072eb8b304f521eb4
Brianojunga <brianojunga3@gmail.com>
2026-02-04 23:54:29 +0300

    Add unique constraints for the db

--- products/models.py
+++ products/models.py
@@ -6,16 +6,26 @@ class Category(models.Model):
     name = models.CharField(max_length=20)
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, related_name='categories')
 
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["vendor", "name"],
+                name="unique_category_per_vendor"
+            )
+        ]
+
     def __str__(self):
         return self.name
 
-class Products(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(null=False, blank=False)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, related_name='products')
     photo = models.ImageField(upload_to='product_photos/', null=True, blank=True)
+    discount = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, default=0)
+    quantity = models.PositiveIntegerField(default=1, null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
