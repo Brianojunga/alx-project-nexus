a986757735a6033d6186447f8c71046aebbf13fc
Brianojunga <brianojunga3@gmail.com>
2026-02-06 23:13:53 +0300

    Add more main url paths

--- core/urls.py
+++ core/urls.py
@@ -33,6 +33,9 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('accounts.urls')),
+    path('api/', include('cart.urls')),
+    path('api/', include('products.urls')),
+    path('api/', include('payments.urls')),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
