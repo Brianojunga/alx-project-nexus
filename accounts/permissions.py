4f0d35929a7946dacf1d9fa6a12be8f64b5c85f7
Brianojunga <brianojunga3@gmail.com>
2026-02-07 20:45:13 +0300

    Update permissions to cover both admin and agent

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -1,5 +1,7 @@
 from rest_framework.permissions import BasePermission
 from accounts.services.has_role import has_role
+from accounts.models import Membership
+from rest_framework.permissions import SAFE_METHODS
     
 class IsPlatformAdmin(BasePermission):
     def has_permission(self, request, view):
@@ -19,3 +21,18 @@ class IsPlatformAgent(BasePermission):
 class IsVendorAgent(BasePermission):
     def has_permission(self, request, view):
         pass
+
+
+class IsVendorAdminOrAgent(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+
+        if not request.user or not request.user.is_authenticated:
+            return False
+
+        return Membership.objects.filter(
+            user=request.user,
+            vendor__slug=view.kwargs["vendor_slug"],
+            role__in=["vendor_admin", "vendor_agent"]
+        ).exists()
