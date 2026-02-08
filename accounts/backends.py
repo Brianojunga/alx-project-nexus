344d71e96dcebc2f932119a17e20e65f849b5659
Brianojunga <brianojunga3@gmail.com>
2026-02-09 00:54:18 +0300

    update backends

--- accounts/backends.py
+++ accounts/backends.py
@@ -1,12 +1,9 @@
 # accounts/backends.py
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
-from django.db.models import Q
 
 class EmailBackend(ModelBackend):
-    """
-    Custom authentication backend that allows users to log in using their email address.
-    """
+    #Custom authentication backend that allows users to log in using their email address.
     def authenticate(self, request, username=None, password=None, email=None, **kwargs):
         UserModel = get_user_model()
         
